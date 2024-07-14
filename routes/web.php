b3f3158137eb613a2a611b7fdfaca4cbb03b79ec
blaspin <daniilschmakov@yandex.ru>
2024-07-14 20:10:08 +0500

    create API routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TrackController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\CustomerController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+    'track' => TrackController::class,
+    'employee' => EmployeeController::class,
+    'customer' => CustomerController::class,
+]);
